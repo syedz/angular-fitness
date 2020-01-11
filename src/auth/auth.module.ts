ec434d43609fc14d87b1f64b8531a5f264dfe82e
Zeeshan Syed <zsyed89@gmail.com>
2020-01-11 14:37:00 -0500

    Updated to @angular/fire, and created AuthService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes, Router } from "@angular/router";
 
+// third-party modules
+import { AngularFireModule, FirebaseAppConfig, FirebaseApp } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+// shared modules
+import { SharedModule } from './shared/shared.module';
+
 export const ROUTES: Routes = [
     {
         path: 'auth',
@@ -13,10 +21,24 @@ export const ROUTES: Routes = [
     }
 ];
 
+export const firebaseConfig: FirebaseAppConfig = {
+    apiKey: "AIzaSyBzl4PHMSJn_GfzU-AbENhKP1y2T_XyB9E",
+    authDomain: "fitness-app-54c2e.firebaseapp.com",
+    databaseURL: "https://fitness-app-54c2e.firebaseio.com",
+    projectId: "fitness-app-54c2e",
+    storageBucket: "fitness-app-54c2e.appspot.com",
+    messagingSenderId: "180176854908",
+    appId: "1:180176854908:web:dd6de98dd19a09a56e0ef9"
+  }
+
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule.forChild(ROUTES)
+        RouterModule.forChild(ROUTES),
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFireAuthModule,
+        AngularFireDatabaseModule,
+        SharedModule.forRoot(), // Need to call forRoot() to avoid duplicate instance of AuthService
     ],
 })
 export class AuthModule {}
