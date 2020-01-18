8aca7f1ae919050d6786ed3e14f7f77a2e7d7094
Zeeshan Syed <zsyed89@gmail.com>
2020-01-18 18:20:18 -0500

    Created MealService and subscribing to Observable in meals.component

---
+++ src/health/shared/services/meals/meals.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+
+import { Store } from 'store';
+
+import { Observable } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+
+import { AuthService } from '../../../../auth/shared/services/auth/auth.service';
+
+export interface Meal {
+    name: string,
+    ingredients: string[],
+    timestamp: number,
+    $key: string,
+    $exists: () => boolean
+}
+
+@Injectable()
+export class MealsService {
+    meals$ = this.db.list(`meals/${this.uid}`)
+        .snapshotChanges().pipe(
+            tap((next) => this.store.set('meals', next))
+        );
+
+    constructor(
+        private store: Store,
+        private db: AngularFireDatabase,
+        private authService: AuthService
+    ) {}
+    
+    get uid() {
+        return this.authService.user.uid;
+    }
+}
