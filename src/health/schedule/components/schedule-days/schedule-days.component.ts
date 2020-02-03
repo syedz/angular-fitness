3dc8c564dc0acc615a9590077583d49885420994
Zeeshan Syed <zsyed89@gmail.com>
2020-02-02 22:32:44 -0500

    Created calendar and week toggle functionality

--- src/health/schedule/components/schedule-days/schedule-days.component.ts
+++ src/health/schedule/components/schedule-days/schedule-days.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
     selector: 'schedule-days',
+    changeDetection: ChangeDetectionStrategy.OnPush,
     styleUrls: ['schedule-days.component.scss'],
     template: `
         <div class="days">
-            
+            <button
+                type="button"
+                class="day"
+                *ngFor="let day of days; index as i"
+                (click)="selectDay(i)">
+                <span [class.active]="i === selected">
+                    {{ day }}
+                </span>
+            </button>
         </div>
     `
 })
 export class ScheduleDaysComponent {
+
+    days = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
+
+    @Input()
+    selected: number;
+
+    @Output()
+    select = new EventEmitter<number>();
+
     constructor() {}
+
+    selectDay(index: number) {
+        this.select.emit(index);
+    }
 }
