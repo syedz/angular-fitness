efd7f41c6fe9248bbece5df921ae669bbe9415dc
Zeeshan Syed <zsyed89@gmail.com>
2020-02-16 12:46:07 -0500

    Changed CSS class

--- src/app/components/app-nav/app-nav.component.ts
+++ src/app/components/app-nav/app-nav.component.ts
@@ -6,7 +6,7 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
     styleUrls: ['app-nav.component.scss'],
     template: `
         <div class="app-nav">
-            <div class="warpper">
+            <div class="wrapper">
                 <a routerLink="schedule" routerLinkActive="active">Schedule</a>
                 <a routerLink="meals" routerLinkActive="active">Meals</a>
                 <a routerLink="workouts" routerLinkActive="active">Workout</a>
