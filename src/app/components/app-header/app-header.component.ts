e3a327b004724dfacafd02143dff659ecc6966ca
Zeeshan Syed <zsyed89@gmail.com>
2020-02-16 12:59:17 -0500

    Removed stuff

--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -9,7 +9,6 @@ import { User } from '../../../auth/shared/services/auth/auth.service';
     template: `
         <div class="app-header">
             <div class="wrapper">
-                <img src="/img/logo.svg" />
                 <div 
                     class="app-header__user-info"
                     *ngIf="user?.authenticated">
